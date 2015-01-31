dc85c490ccb23d526a2c6defa8fdbd2cf7ec93cc
evashork <evashork@naver.com>
2015-01-31 15:11:43 +0900

    t

---
+++ JAVA기초/Scanner.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class ScannerTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("이름 : "); //println 줄바꿈,
+		String name = sc.next();		
+		System.out.println("name = " + name);
+		
+		System.out.print("나이 : ");
+		int age = sc.nextInt();
+		System.out.println("age = " + age);
+	}
+
+}
